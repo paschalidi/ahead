a96f53fe3b7dcf859a4f93dd80a7d9237d0303c7
paschalidi <paschalidi@outlook.com>
2021-02-01 17:00:26 +0000

    chore: flattens the folder structure

--- src/Button/styles.ts
+++ src/Button/styles.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { colors } from "../constants/colors";
 
 export const ButtonStyles = styled.div`
   button:disabled,
