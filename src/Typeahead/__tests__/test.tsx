664f2c7cce03e3e7177ecc76231ea4848d49ca76
paschalidi <paschalidi@outlook.com>
2021-02-02 17:42:19 +0000

    test: renames tests and writes extra scenarios

--- src/Typeahead/__tests__/test.tsx
+++ src/Typeahead/__tests__/test.tsx
@@ -17,7 +17,7 @@ const setup = () => {
 
 const userInputCases = ["plu", "plum", "avo", "app", "a"];
 
-test("should render input", () => {
+test("you should see the rendered input", () => {
   // given
   const { typeahead } = setup();
 
@@ -38,21 +38,6 @@ test("should render input", () => {
   `);
 });
 
-userInputCases.forEach((userInput) =>
-  test(`should show the list when there types ${userInput}`, () => {
-    // given
-    const { inputElement, typeahead, getByTestId } = setup();
-
-    // when
-    fireEvent.change(inputElement, { target: { value: userInput } });
-
-    // then
-    const listElement = getByTestId("suggestions-list");
-    expect(typeahead).toMatchSnapshot();
-    expect(listElement).toBeInTheDocument();
-  })
-);
-
 // todo testing scenarios
 //  type in and select with click, make sure you can erase after
 //  type in and select with enter, make sure you can erase after
@@ -60,3 +45,158 @@ userInputCases.forEach((userInput) =>
 //  type in and go till the end of the list. then on the top
 //  type in then move with key then type again. make sure selection reseted
 //  type in and then hover over make sure input has the text you hovered
+
+test(`you should see the suggestion you clicked on the input`, () => {
+  // given
+  const { inputElement, typeahead, getByLabelText } = setup();
+
+  // when
+  fireEvent.change(inputElement, { target: { value: "a" } });
+  // and
+  fireEvent(
+    getByLabelText("Açaí"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+
+  // then
+  expect(inputElement.value).toBe("Açaí");
+  expect(typeahead).toMatchInlineSnapshot(`
+    <div
+      data-testid="typeahead"
+    >
+      <input
+        aria-label="typeahead-input"
+        class="sc-bdfBwQ juMTdC"
+        placeholder="Type..."
+        type="text"
+        value="Açaí"
+      />
+    </div>
+  `);
+});
+
+test(`after you already made a selection you should be able to type in the input`, () => {
+  // given
+  const { inputElement, typeahead, getByLabelText } = setup();
+
+  // when
+  fireEvent.change(inputElement, { target: { value: "a" } });
+  // and
+  fireEvent(
+    getByLabelText("Açaí"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  // and
+  fireEvent.change(inputElement, { target: { value: "avocado" } });
+
+  // then
+  expect(inputElement.value).toBe("avocado");
+  expect(typeahead).toMatchInlineSnapshot(`
+    <div
+      data-testid="typeahead"
+    >
+      <input
+        aria-label="typeahead-input"
+        class="sc-bdfBwQ juMTdC"
+        placeholder="Type..."
+        type="text"
+        value="avocado"
+      />
+      <ul
+        class="sc-gsTCUz MsIIl"
+        data-testid="suggestions-list"
+      >
+        <li>
+          <button
+            aria-label="Avocado"
+            class="sc-dlfnbm cHfzSk"
+            type="button"
+          >
+            <span>
+              
+              <span
+                class="coloredText"
+              >
+                Avocado
+              </span>
+            </span>
+            <span />
+          </button>
+        </li>
+      </ul>
+    </div>
+  `);
+});
+
+test(`after you already made a selection you should be able to type _and_ make another selection`, () => {
+  // given
+  const { inputElement, typeahead, getByLabelText, queryByTestId } = setup();
+
+  // when
+  fireEvent.change(inputElement, { target: { value: "a" } });
+  // and
+  fireEvent(
+    getByLabelText("Açaí"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  // and
+  fireEvent.change(inputElement, { target: { value: "avoca" } });
+  // and
+  fireEvent(
+    getByLabelText("Avocado"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+
+  // then
+  expect(inputElement.value).toBe("Avocado");
+  expect(typeahead).toMatchInlineSnapshot(`
+    <div
+      data-testid="typeahead"
+    >
+      <input
+        aria-label="typeahead-input"
+        class="sc-bdfBwQ juMTdC"
+        placeholder="Type..."
+        type="text"
+        value="Avocado"
+      />
+    </div>
+  `);
+});
+
+test(`you should be able to search using the fuzzy search`, () => {
+  // given
+  const { inputElement, getByTestId } = setup();
+
+  // when
+  fireEvent.change(inputElement, { target: { value: "buas" } });
+  // then
+  expect(getByTestId("suggestions-list")).toHaveTextContent("Buddha's hand");
+});
+
+// userInputCases.forEach((userInput) =>
+//   test(`you should see a list of suggestions when user types ${userInput}`, () => {
+//     // given
+//     const { inputElement, typeahead, getByTestId } = setup();
+//
+//     // when
+//     fireEvent.change(inputElement, { target: { value: userInput } });
+//
+//     // then
+//     const listElement = getByTestId("suggestions-list");
+//     expect(typeahead).toMatchSnapshot();
+//     expect(listElement).toBeInTheDocument();
+//   })
+// );
