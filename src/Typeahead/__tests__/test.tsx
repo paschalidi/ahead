7c5d532df9177a1ccf689a605cf6645e681d0226
paschalidi <paschalidi@outlook.com>
2021-02-02 16:56:07 +0000

    chore: removes inline snapshot

--- src/Typeahead/__tests__/test.tsx
+++ src/Typeahead/__tests__/test.tsx
@@ -17,31 +17,33 @@ const userInputCases = ["plu", "plum", "avo", "app", "a"];
 test("should render input", () => {
   // given
   const { inputElement } = setup();
+
   // then
   expect(inputElement).toBeInTheDocument();
-  expect(inputElement).toMatchInlineSnapshot(`
-    <input
-      aria-label="typeahead-input"
-      class="sc-bdfBwQ juMTdC"
-      placeholder="Type..."
-      type="text"
-      value=""
-    />
-  `);
+  expect(inputElement).toMatchSnapshot();
 });
 
 userInputCases.forEach((userInput) =>
   test(`should show the list when there types ${userInput}`, () => {
     // given
     const { inputElement, getByTestId } = setup();
-    // when
-    fireEvent.change(inputElement, { target: { value: userInput } });
     const listElement = getByTestId("suggestions-list");
 
+    // when
+    fireEvent.change(inputElement, { target: { value: userInput } });
+
+    // then
     expect(inputElement).toBeInTheDocument();
     expect(inputElement).toMatchSnapshot();
-
     expect(listElement).toBeInTheDocument();
     expect(listElement).toMatchSnapshot();
   })
 );
+
+// todo testing scenarios
+//  type in and select with click, make sure you can erase after
+//  type in and select with enter, make sure you can erase after
+//  type in and make sure matching is case insensitive
+//  type in and go till the end of the list. then on the top
+//  type in then move with key then type again. make sure selection reseted
+//  type in and then hover over make sure input has the text you hovered
