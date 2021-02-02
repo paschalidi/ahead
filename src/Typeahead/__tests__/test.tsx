2d00399c564417249c97f84b69990cfe32ba92d7
paschalidi <paschalidi@outlook.com>
2021-02-02 17:06:40 +0000

    test: updates snapshots

--- src/Typeahead/__tests__/test.tsx
+++ src/Typeahead/__tests__/test.tsx
@@ -5,8 +5,11 @@ import { Typeahead } from "../index";
 
 const setup = () => {
   const utils = render(<Typeahead />);
+  const typeahead = utils.getByTestId("typeahead");
   const inputElement = utils.getByLabelText("typeahead-input");
+
   return {
+    typeahead,
     inputElement,
     ...utils,
   };
@@ -16,27 +19,37 @@ const userInputCases = ["plu", "plum", "avo", "app", "a"];
 
 test("should render input", () => {
   // given
-  const { inputElement } = setup();
+  const { typeahead } = setup();
 
   // then
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement).toMatchSnapshot();
+  expect(typeahead).toBeInTheDocument();
+  expect(typeahead).toMatchInlineSnapshot(`
+    <div
+      data-testid="typeahead"
+    >
+      <input
+        aria-label="typeahead-input"
+        class="sc-bdfBwQ juMTdC"
+        placeholder="Type..."
+        type="text"
+        value=""
+      />
+    </div>
+  `);
 });
 
 userInputCases.forEach((userInput) =>
   test(`should show the list when there types ${userInput}`, () => {
     // given
-    const { inputElement, getByTestId } = setup();
-    const listElement = getByTestId("suggestions-list");
+    const { inputElement, typeahead, getByTestId } = setup();
 
     // when
     fireEvent.change(inputElement, { target: { value: userInput } });
 
     // then
-    expect(inputElement).toBeInTheDocument();
-    expect(inputElement).toMatchSnapshot();
+    const listElement = getByTestId("suggestions-list");
+    expect(typeahead).toMatchSnapshot();
     expect(listElement).toBeInTheDocument();
-    expect(listElement).toMatchSnapshot();
   })
 );
 
